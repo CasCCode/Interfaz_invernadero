f7014e10cddd81e3b6979d63134f884e1f844e3d
“CasCCode” <estebancastro@javerianacali.edu.co>
2025-03-16 23:51:26 -0500

    MOD: agregados botones y barra lateral de opciones del config. Los botones no tienen funcionalidades

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,46 @@
+import { useState } from "react";
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import GreenhouseCard from '../components/GreenhouseCard';
 
 import { FaArrowLeft, FaCog } from 'react-icons/fa';
-import { Container, Card, Row, Col, Image } from "react-bootstrap";
+import { Container, Image, Button, Offcanvas } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import profileImg from '../assets/profileImg.jpg'; // Ajuste en la ruta
 
 function HomePage() {
   const greenhouses = useSelector(state => state.greenhouse.greenhouses);
+  const [showMenu, setShowMenu] = useState(false);
+  
   return (
     <div className="container">
       <Container className="p-3 bg-success text-white text-center rounded-bottom">
         <div className="text-center bg-success text-white p-4 rounded">
           <div className="d-flex justify-content-between">
-            <FaArrowLeft className="fs-1"/> {/* Icono de volver */}
-            <FaCog className="fs-1"/> {/* Icono de ajustes */}
+            
+            <Button variant="outline-light" className=""> {/* Botón de volver */}
+              <FaArrowLeft size={24}/> {/* Icono de flecha */}
+            </Button>
+
+            <Button variant="outline-light" className=""  
+              onClick={() => setShowMenu(true)}> {/* Botón de ajustes */}
+              <FaCog className="fs-1"/> {/* Icono de ajustes */}
+            </Button>
+
+            {/* Menú lateral Offcanvas */}
+            <Offcanvas show={showMenu} onHide={() => setShowMenu(false)} placement="end">
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Configuración</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <p>Agregar aquí las opciones de conf</p>
+                <Button variant="light" onClick={() => console.log("Opción 1 seleccionada")}>
+                  Opción 1
+                </Button>
+              </Offcanvas.Body>
+            </Offcanvas>
+            
           </div>
           <Image src={profileImg} roundedCircle width={100} height={100} className="my-3" />
           <h1>¡Bienvenido usuario!</h1>
